ed4e308f7c898571580fdbe6e018e614542bea23
saad vadanagara <saadvadanagara@gmail.com>
2023-01-21 17:15:17 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
+import './Navbar.css';
 function Navbar() {
     return (
         <div>
-            <ul>
-                <li><a class="active" href="#home">Home</a></li>
-                <li><a href="#news">News</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#about">About</a></li>
-            </ul>
+            <nav className="navbar">
+                <div className="name-title">Saad Vadnagara</div>
+                <div className="navbar-links">
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">Projects</a></li>
+                        <li><a href="/">Skills</a></li>
+                        <li><a href="/">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     );
 }
